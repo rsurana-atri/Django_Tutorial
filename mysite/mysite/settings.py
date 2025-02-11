fb104fb97924e9e21f3035223e002240c5404114
rsurana-atri <rsurana@atrihub.io>
2025-02-11 14:39:23 -0800

    feature(mysite): Chapter 2, models and the admin site
    Added Question and Choice models to support the polls website, as well as model access
    
    [ICTRAINING-1796]

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'template1',
         'USER': 'rsurana',
         'HOST': 'localhost',
     }
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
